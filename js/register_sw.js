752e0d4ca4e5eccfba3efa3e98b7a737feb41251
Moamen <frontcodeme@gmail.com>
2018-08-24 16:32:11 +0200

    Registering ServiceWorker

--- js/register_sw.js
+++ js/register_sw.js
@@ -1,8 +1,9 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('js/register_sw.js')
     .then(registration => {
         console.log(`Registration successful, scope is ${registration.scope}`);
     }).catch(error => {
         console.log(`Service worker registration failed, error: ${error}`);
     });
 }
+importScripts('js/register_sw.js');  // Update path to match your own setup.
